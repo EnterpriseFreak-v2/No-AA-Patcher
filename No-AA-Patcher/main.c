c9c1e834017d2220ad9c67292f17a1501ccb0a90
EnterpriseFreak-v2 <30931394+EnterpriseFreak-v2@users.noreply.github.com>
2021-09-01 22:02:45 +0200

    Code's much better now

--- No-AA-Patcher/main.c
+++ No-AA-Patcher/main.c
@@ -1,62 +1,78 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // I don't care if you consider normal fopen unsafe, Visual Studio.
 
 #include <stdio.h>
-#include <stdint.h>
-#include <malloc.h>
 #include <string.h>
+#include <malloc.h>
 
-char pattern[] = {0x08, 0x08, 0x0a, 0x0c, 0x0a, 0x08, 0x08, 0x00};
-char pattern_new[] = {0x00, 0x00, 0x15, 0x16, 0x15, 0x00, 0x00, 0x00};
+const char ptr_old[] = { 0x08, 0x08, 0x0a, 0x0c, 0x0a, 0x08, 0x08, 0x00 }; // NTSC / PAL progressive "soft" / "aa" rmode
+const char ptr_new[] = { 0x00, 0x00, 0x15, 0x16, 0x15, 0x00, 0x00, 0x00 }; // NTSC / PAL progressive rmode
 
 int main(int argc, char* argv[])
 {
-	uint64_t numItems = 0;
-	uint64_t*	items;
+	FILE *src, *dst;
+	char *buf, *pos;
+	size_t srcSize;
 
-	size_t maindolsize = 0;
-	uint8_t* maindolbuff;
-	FILE* dol;
-
-	dol = fopen(argv[1], "rb");
-	
-	if (!dol)
+	// Make sure we've got a file to work with
+	if (argc < 2)
 	{
-		printf("Unable to open '%s', aborting!\n", argv[1]);
+		printf("No source file specified.\nUsage: ./No-AA-Patcher <source dol> <output dol>\n");
 		return 1;
 	}
 
-	fseek(dol, 0, SEEK_END);
-	maindolsize = ftell(dol);
-	rewind(dol);
-
-	maindolbuff = malloc(maindolsize);
-
-	if (!maindolbuff)
+	// Are we able to open it?
+	if (!(src = fopen(argv[1], "rb")))
 	{
-		printf("Unable to allocate memory buffer, aborting! (Is your system low on memory?)\n");
-		fclose(dol);
+		printf("Unable to open DOL file '%s'!\n", argv[1]);
 		return 1;
 	}
 
-	fread(maindolbuff, maindolsize, 1, dol);
-	fclose(dol);
-
-	for (int i = 0; i < maindolsize - 8; i++)
+	// Try to open the destination file, this can be patched.dol or a filename specified by the user.
+	if (!(dst = fopen(argv[2] != NULL ? argv[2] : "patched.dol", "wb")))
 	{
-		char* loc = (char*)maindolbuff + i;
+		fclose(src);
+		printf("Unable to open output file '%s' for writing!\n", argv[2] != NULL ? argv[2] : "patched.dol");
+		return 1;
+	}
 
-		if (memcmp(loc, pattern, 8) == 0)
+	// Get the size of the source dol and try to allocate enough memory to load it entirely into memory.
+	fseek(src, 0, SEEK_END);
+	srcSize = ftell(src);
+	fseek(src, 0, SEEK_SET);
+	buf = (char*)malloc(srcSize);
+
+	// Confirm that we've actually got a pointer to the memory we requested.
+	if (!buf)
+	{
+		fclose(src);
+		fclose(dst);
+		printf("Unable to allocate %dK memory for processing!\n", (int)srcSize / 1024);
+		return 1;
+	}
+
+	// Read the source file into our buffer
+	fread(buf, srcSize, 1, src);
+
+	// Search for the "soft" / "aa" rmode pattern and replace it, if we find it.
+	for (int i = 0; i < srcSize - sizeof(ptr_old); i++)
+	{
+		pos = (char*)buf + i;
+
+		if (memcmp(pos, ptr_old, 8) == 0)
 		{
-			memcpy(maindolbuff + i, pattern_new, 8);
-			printf("%lx\n", maindolbuff + i);
+			memcpy(pos, ptr_new, 8);
 		}
-		
 	}
 
-	FILE* out = fopen("patched.dol", "wb");
-	fwrite(maindolbuff, maindolsize, 1, out);
-	fclose(out);
+	// Write the modified dol which we currently have in memory to the disk
+	fwrite(buf, srcSize, 1, dst);
+	fflush(dst);
 
+	// Clean up and release all resources we have used.
+	fclose(src);
+	fclose(dst);
+	free(buf);
 
+	printf("And we're done, have a great day!\n");
 	return 0;
 }
