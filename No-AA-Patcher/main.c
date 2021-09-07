2012045610c9c3712403dfaa1460050e9214187f
EnterpriseFreak-v2 <30931394+EnterpriseFreak-v2@users.noreply.github.com>
2021-09-07 07:51:48 +0200

    Allow the user to disable dithering

--- No-AA-Patcher/main.c
+++ No-AA-Patcher/main.c
@@ -7,6 +7,46 @@
 const char ptr_old[] = { 0x08, 0x08, 0x0a, 0x0c, 0x0a, 0x08, 0x08, 0x00 }; // NTSC / PAL progressive "soft" / "aa" rmode
 const char ptr_new[] = { 0x00, 0x00, 0x15, 0x16, 0x15, 0x00, 0x00, 0x00 }; // NTSC / PAL progressive rmode
 
+const char ptr_dithering[] = {0x3C, 0x80, 0xCC, 0x01, 0x38, 0xA0, 0x00, 0x61,
+							  0x38, 0x00, 0x00, 0x00, 0x80, 0xC7, 0x02, 0x20,
+							  0x50, 0x66, 0x17, 0x7A, 0x98, 0xA4, 0x80, 0x00, // The four bytes before this pattern determine if the game should apply dithering or not.
+							  0x90, 0xC4, 0x80, 0x00, 0x90, 0xC7, 0x02, 0x20,
+							  0xB0, 0x07, 0x00, 0x02, 0x4E, 0x80, 0x00, 0x20};
+const char ptr_nodither[] = {0x48, 0x00, 0x00, 0x28};
+
+char patchAA, patchDither;
+
+// Parses argv.
+int parseArgs(char* argv[])
+{
+	int parsed = 0;
+	int current = 1;
+
+	while(argv[current] != NULL)
+	{
+		if (strncmp(argv[current], "--", 2) != 0)
+		{
+			break;
+		}
+
+		if (strcmp(argv[current], "--noaa") == 0)
+		{
+			patchAA = 1;
+			parsed++;
+		}
+
+		if (strcmp(argv[current], "--nodither") == 0)
+		{
+			patchDither = 1;
+			parsed++;
+		}
+
+		current++;
+	}
+
+	return parsed;
+}
+
 int main(int argc, char* argv[])
 {
 	FILE *src, *dst;
@@ -14,24 +54,33 @@ int main(int argc, char* argv[])
 	size_t srcSize;
 
 	// Make sure we've got a file to work with
-	if (argc < 2)
+	if (argc < 3)
 	{
-		printf("No source file specified.\nUsage: ./No-AA-Patcher <source dol> <output dol>\n");
+		printf("Not enough arguments provided!\nUsage: ./No-AA-Patcher <patch(es)> <source dol> <output dol>\n");
+		printf("Patches:\n");
+		printf("--noaa : Removes the anti-aliasing / flicker filter from the DOL.\n");
+		printf("--nodither : Disables dithering in the DOL.");
+		return 1;
+	}
+
+	if (!parseArgs(argv))
+	{
+		printf("No patch specfied! Run this program without any arguments to view a list of patches you can apply!\n");
 		return 1;
 	}
 
 	// Are we able to open it?
-	if (!(src = fopen(argv[1], "rb")))
+	if (!(src = fopen(argv[argc-2], "rb")))
 	{
-		printf("Unable to open DOL file '%s'!\n", argv[1]);
+		printf("Unable to open DOL file '%s'!\n", argv[argc-2]);
 		return 1;
 	}
 
 	// Try to open the destination file, this can be patched.dol or a filename specified by the user.
-	if (!(dst = fopen(argv[2] != NULL ? argv[2] : "patched.dol", "wb")))
+	if (!(dst = fopen(argv[argc-1], "wb")))
 	{
 		fclose(src);
-		printf("Unable to open output file '%s' for writing!\n", argv[2] != NULL ? argv[2] : "patched.dol");
+		printf("Unable to open output file '%s' for writing!\n", argv[argc-2]);
 		return 1;
 	}
 
@@ -54,13 +103,26 @@ int main(int argc, char* argv[])
 	fread(buf, srcSize, 1, src);
 
 	// Search for the "soft" / "aa" rmode pattern and replace it, if we find it.
-	for (int i = 0; i < srcSize - sizeof(ptr_old); i++)
+	for (int i = 0; i < srcSize - sizeof(ptr_dithering); i++)
 	{
 		pos = (char*)buf + i;
 
-		if (memcmp(pos, ptr_old, 8) == 0)
+		// If wanted by the user replace the "soft" / "aa" rmode with the normal progressive one.
+		if (patchAA)
 		{
-			memcpy(pos, ptr_new, 8);
+			if (memcmp(pos, ptr_old, 8) == 0)
+			{
+				memcpy(pos, ptr_new, 8);
+			}
+		}
+
+	// If wanted by the user remove the dithering filter from the game.
+		if (patchDither)
+		{
+			if (memcmp(pos, ptr_dithering, 40) == 0)
+			{
+				memcpy(pos - 4, ptr_nodither, 4);
+			}
 		}
 	}
 
