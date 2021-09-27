1d36998b200bec5c62a89aa692252f9a31ea71f8
EnterpriseFreak-v2 <kevinlenzmozer@gmail.com>
2021-09-27 20:30:18 +0200

    Make sure there's seven 0x06 bytes preceeding the deflicker filter pattern. This should prevent the main.dol of some games (the PAL version of Super Paper Mario is one such game) crashing randomly.

--- No-AA-Patcher/deflicker.c
+++ No-AA-Patcher/deflicker.c
@@ -10,6 +10,7 @@ const char ptr_old_mkwii1[] = { 0x07, 0x07, 0x0c, 0x0c, 0x0c, 0x07, 0x07 }; // M
 const char ptr_old_mkwii2[] = { 0x05, 0x05, 0x0f, 0x0e, 0x0f, 0x05, 0x05 }; // Mario Kart Wii
 const char ptr_old_galaxy[] = { 0x20, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; // Super Mario Galaxy 1 / 2
 const char ptr_new_nofilt[] = { 0x00, 0x00, 0x15, 0x16, 0x15, 0x00, 0x00 }; // Desired filter pattern (no filter)
+const char ptr_preceding[] = { 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06 }; // Pattern that preceeds the filter pattern
 
 void applyDeflickerPatch(char* buffer, size_t bufsize)
 {
@@ -20,23 +21,23 @@ void applyDeflickerPatch(char* buffer, size_t bufsize)
 	{
 		position = (char*)buffer + i;
 
-		if (memcmp(position, ptr_old_progSf, 7) == 0)
+		if (memcmp(position, ptr_old_progSf, 7) == 0 && memcmp(position - 7, ptr_preceding, 7) == 0)
 		{
 			memcpy(position, ptr_new_nofilt, 7);
 		}
-		else if (memcmp(position, ptr_old_progAa, 7) == 0)
+		else if (memcmp(position, ptr_old_progAa, 7) == 0 && memcmp(position - 7, ptr_preceding, 7) == 0)
 		{
 			memcpy(position, ptr_new_nofilt, 7);
 		}
-		else if (memcmp(position, ptr_old_mkwii1, 7) == 0)
+		else if (memcmp(position, ptr_old_mkwii1, 7) == 0 && memcmp(position - 7, ptr_preceding, 7) == 0)
 		{
 			memcpy(position, ptr_new_nofilt, 7);
 		}
-		else if (memcmp(position, ptr_old_mkwii2, 7) == 0)
+		else if (memcmp(position, ptr_old_mkwii2, 7) == 0 && memcmp(position - 7, ptr_preceding, 7) == 0)
 		{
 			memcpy(position, ptr_new_nofilt, 7);
 		}
-		else if (memcmp(position, ptr_old_galaxy, 7) == 0)
+		else if (memcmp(position, ptr_old_galaxy, 7) == 0 && memcmp(position - 7, ptr_preceding, 7) == 0)
 		{
 			memcpy(position, ptr_new_nofilt, 7);
 		}
